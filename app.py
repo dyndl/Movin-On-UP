51bef26d45440b79ca4216dc620e0e8ab0f011a9
Duane M. Lee <duane.m.lee@gmail.com>
2019-06-23 18:36:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ mapbox_access_token = 'pk.eyJ1IjoiZHluZGwiLCJhIjoiY2p4M2gyYm9wMDBzbDRhbmxzYWMya2
 # loading data
 path = '/Users/dmlee/Desktop/Insight_DS/data/'
 #path = r'C:\DRO\DCL_rawdata_files' # use your path
-all_files = glob.glob(path + 'ZILLOW/Z*_MSPAHforecast.csv')
+all_files = glob.glob(path + 'ZILLOW2/Z*_MSPAHforecast.csv')
 
 li = []
 
